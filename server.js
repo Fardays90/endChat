45253013cf20e35f6392bc0f43e112197afae128
Fardays90 <165808254+Fardays90@users.noreply.github.com>
2024-12-14 19:57:42 +0600

    dm

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const io = new Server(httpServer);
 const PORT = 4000;
 app.use(express.static('public'));
 app.use('/src', express.static('src'));
+const users = {};
 io.on('connect', (socket) => {
     let userName;
     socket.on('username',(username) => {
         userName = username;
         console.log(userName+' connected!');
+        users[username] = socket.id;
         io.emit('userJoined', userName+' joined the chat.');
     })
     socket.on('chatMessage', (msg) => {
@@ -20,6 +22,18 @@ io.on('connect', (socket) => {
     socket.on('disconnect', () => {
         console.log('a user disconnected');
         io.emit('userDis', userName+' left the chat.');
+    })
+    socket.on('dm', ({personToSend, msg }) => {
+        const personId = users[personToSend];
+        if(personId){
+            io.to(personId).emit('dm', {
+                from: userName,
+                msg
+            })
+        }
+        else{
+            socket.emit('dm', 'person is not online');
+        }
     })
 })
 
